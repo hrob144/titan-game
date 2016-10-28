bd1b03ad70813e9c12e120c9d43b43194baacbd4
Hunter Roberts <hroberts144@gmail.com>
2016-10-28 11:49:37 -0400

    Add text.js file to streamline game logic

---
+++ js/text.js
@@ -0,0 +1,20 @@
+var wakeUpTxt = 
+                '<br/>' +
+                '=============================================================================================================<br/>' +
+                'Log date: October 26th, 2341<br/>' +
+                'Coordinates: 2.3AU from Arrival Sector Bravo<br/>' +
+                'Aboard spacecraft Titan XIV<br/>' +
+                '=============================================================================================================<br/>' +
+                '<br/>' +
+                'You wake up in your bunk aboard the Heart of Gold from a strange dream. One that you\'ve had countless times since your journey began nearly two and a half years ago.br/>' +
+                '<br/>' +
+                'This time was no different ... plunged in a deep sea, you hear the sound of voices under the waves.<br/>' +
+                'Hundreds, if not thousands of voices, all crying out for someone--anyone--to answer their plea.<br/>' +
+                'But as you finally break the surface of the water, the cries are extinguished like a flickering candle.<br/>' +
+                '<br/>' +
+                'You look at your watch. It\'s the middle of the sleep schedule, and you doubt that anyone else onboard the transport is awake.<br/>' +
+                '<br/>';
+
+var wakeUpPromptTxt = 
+                'It\'s time for your first decision. Do you want to go back to sleep, or get out of bed and stretch your legs?<br/>' +
+                '(a) sleep, (b) get up<br/>';
